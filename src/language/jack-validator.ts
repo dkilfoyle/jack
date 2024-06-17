5cf94c1412ab233d72c03f78c9a1bf1c613ae084
Dean Kilfoyle <dkilfoyle@xtra.co.nz>
2024-06-17 22:56:54 +1200

    first

---
+++ src/language/jack-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { JackAstType, Person } from './generated/ast.js';
+import type { JackServices } from './jack-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: JackServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.JackValidator;
+    const checks: ValidationChecks<JackAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class JackValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
---
+++ static/setupExtended.js
@@ -0,0 +1,62 @@
+import { addMonacoStyles, defineUserServices, MonacoEditorLanguageClientWrapper } from './bundle/index.js';
+import { configureWorker } from './setup.js';
+
+addMonacoStyles('monaco-editor-styles');
+
+export const setupConfigExtended = () => {
+    const extensionFilesOrContents = new Map();
+    const languageConfigUrl = new URL('../language-configuration.json', window.location.href);
+    const textmateConfigUrl = new URL('../syntaxes/jack.tmLanguage.json', window.location.href);
+    extensionFilesOrContents.set('/language-configuration.json', languageConfigUrl);
+    extensionFilesOrContents.set('/jack-grammar.json', textmateConfigUrl);
+
+    return {
+        wrapperConfig: {
+            serviceConfig: defineUserServices(),
+            editorAppConfig: {
+                $type: 'extended',
+                languageId: 'jack',
+                code: `// jack is running in the web!`,
+                useDiffEditor: false,
+                extensions: [{
+                    config: {
+                        name: 'jack-web',
+                        publisher: 'generator-langium',
+                        version: '1.0.0',
+                        engines: {
+                            vscode: '*'
+                        },
+                        contributes: {
+                            languages: [{
+                                id: 'jack',
+                                extensions: [
+                                    '.jack'
+                                ],
+                                configuration: './language-configuration.json'
+                            }],
+                            grammars: [{
+                                language: 'jack',
+                                scopeName: 'source.jack',
+                                path: './jack-grammar.json'
+                            }]
+                        }
+                    },
+                    filesOrContents: extensionFilesOrContents,
+                }],                
+                userConfiguration: {
+                    json: JSON.stringify({
+                        'workbench.colorTheme': 'Default Dark Modern',
+                        'editor.semanticHighlighting.enabled': true
+                    })
+                }
+            }
+        },
+        languageClientConfig: configureWorker()
+    };
+};
+
+export const executeExtended = async (htmlElement) => {
+    const userConfig = setupConfigExtended();
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    await wrapper.initAndStart(userConfig, htmlElement);
+};
